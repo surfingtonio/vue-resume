fbc83da57395b098dc283f30e855e9d8732aafbf
Danting, Marc <marc@danting.ca>
2020-09-25 16:57:01 -0700

    Fixed compatibility bug with some mobile browsers.

--- src/components/Section.vue
+++ src/components/Section.vue
@@ -16,9 +16,13 @@ export default {
   },
   computed: {
     defaultClassNames: function () {
-      return this.title === undefined
-        ? null
-        : this.title.replaceAll(" ", "-").toLowerCase();
+      // This component does not render on mobile devices
+      // when using a version of Chrome less than 85.0.4183.127
+      // or Samsung Internet version 12.1.2.5 apparently
+      // because of the call to the javascript replaceAll() method.
+      let words =
+        this.title === undefined ? [] : this.title.toLowerCase().split(/\s/);
+      return words.join("-");
     },
   },
 };
